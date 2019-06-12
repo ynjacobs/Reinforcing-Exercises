468c471d60b347e6380b312b625afa2f8e08841c
ynjacobs <ytj64611@gamil.com>
2019-06-12 14:10:25 -0400

    adds person to steps and adds a comittee member

---
+++ June11.py
@@ -0,0 +1,48 @@
+project = { 
+  'committee': ["Stella", "Salma", "Kai"],
+  'title': "Very Important Project",
+  'due_date': "December 14, 2019",
+  'id': "3284",
+  'steps': [
+    {'description': "conduct interviews",
+     'due_date': "August 1, 2018"
+    },
+    {'description': "code of conduct",
+     'due_date': "January 1, 2018"
+    },
+    {'description': "compile results",
+     'due_date': "November 10, 2018"
+    },
+    {'description': "version 1",
+     'due_date': "January 15, 2019"
+    },
+    {'description': "revisions",
+     'due_date': "March 30, 2019"
+    },
+    {'description': "version 2",
+     'due_date': "July 12, 2019"
+    },
+    {'description': "final edit",
+     'due_date': "October 1, 2019"
+    },
+    {'description': "final version",
+     'due_date': "November 20, 2019"
+    },
+    {'description': "Wrap up",
+     'due_date': "December 1, 2019"
+    }
+  ]
+}
+
+
+tasks = project['steps']
+members = project['committee']
+counter = 0
+for task in tasks:
+    index = counter % len(members)
+    task['person']  = members[int(index)]
+    counter += 1
+
+for task in tasks:
+    print('->', task)
+    
